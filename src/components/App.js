0a503a5cbe6aa739205bbd64df7d9583ae85b080
Martin <skydiver@users.noreply.github.com>
2017-10-07 20:12:02 -0300

    * delete snippets

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
     constructor() {
         super();
         this.addSnippet = this.addSnippet.bind(this);
+        this.removeSnippet = this.removeSnippet.bind(this);
         this.state = {
             snippets: {},
         };
@@ -17,11 +18,14 @@ class App extends React.Component {
         return (
             <div className="container">
                 <div id="snippets-list">
-                    <ul className="list-of-fishes">
+                    <ul>
                     {
                         Object
                             .keys(this.state.snippets)
-                            .map(key => <Snippet key={key} index={key} details={this.state.snippets[key]} />)
+                            .map(key => <Snippet key={key} index={key}
+                                details={this.state.snippets[key]}
+                                removeSnippet={this.removeSnippet}
+                            />)
                     }
                   </ul>
                 </div>
@@ -39,6 +43,12 @@ class App extends React.Component {
         const timestamp = Date.now();
         snippets[`snippet-${timestamp}`] = snippet;
         // set state
+        this.setState({ snippets });
+    }
+
+    removeSnippet(key) {
+        const snippets = { ...this.state.snippets };
+        snippets[key] = null;
         this.setState({ snippets });
     }
 
--- src/components/Snippet.js
+++ src/components/Snippet.js
@@ -3,12 +3,13 @@ import React from 'react';
 class Snippet extends React.Component {
 
     render() {
-        const { details } = this.props;
+        const { details, index } = this.props;
         return (
-            <li className="menu-fish">
+            <li className="snippet-item">
                 <p>{ details.snippet }</p>
+                <button className="snippet-delete" onClick={() => this.props.removeSnippet(index)}>&times;</button>
             </li>
-        )
+        );
     }
 }
 
